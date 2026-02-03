d8c217eed51370430de73a676a1a12834083d71a
AayushShetty01 <113285327+AayushShetty01@users.noreply.github.com>
2026-02-03 09:41:07 +0530

    Update and rename preprocess.py to src/preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,19 +1,14 @@
-import nltk
 import re
-import pandas as pd
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
 nltk.download('stopwords')
 
+ps = PorterStemmer()
+
 def clean_text(text):
     text = re.sub('[^a-zA-Z]', ' ', text)
     text = text.lower().split()
-    ps = PorterStemmer()
     text = [ps.stem(word) for word in text if word not in stopwords.words('english')]
     return ' '.join(text)
-
-def preprocess_data(file_path):
-    df = pd.read_csv(file_path)
-    df['cleaned_review'] = df['review'].apply(clean_text)
-    return df
